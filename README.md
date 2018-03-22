4cd4a94562405b9b5f621a2e0c05c9175e9d234f
Randy Reitz <rsreitz@outlook.com>
2018-03-22 09:45:27 -0400

    Cleaned up comments and pseudo-coded future functionality.

--- README.md
+++ README.md
@@ -1,43 +1,41 @@
 # rps-multiplayer
-A rock-paper-scissors multiplayer web realtime game. (class)
+A realtime rock-paper-scissors multiplayer web game. (class)
 
 ### Overview
 
-In this assignment, you'll indeed create another Rock Paper Scissors game. The catch? You're going to make this an online multiplayer game, all with the help of Firebase (and the rest of your web development repertoire)!
+  * Only two users can play at the same time.
 
+  * Both players pick either `rock`, `paper` or `scissors`. After both players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
 
-TODO:
+  * The game will track each player's wins and losses.
+
+## TODO:
 - ~~User(s) sign in~~
-- save user ID in the browser...
 - ~~Users choose to join a game~~
 - ~~Users are matched up~~
 - ~~Game begins~~
 - ~~Each user presented RPS buttons~~
 - ~~User selects RPS button~~
 - ~~Response is sent back to server~~
-- Server waits until both responses received 
-    ("waiting" state can be sent)
-- Server compares responses
-- Winner declared
-- Score updated
+- ~~Server waits until both responses received~~
+- ~~Server compares responses~~
+- ~~Winner declared~~
+- ~~Score updated~~
 - Best of 3 wins
-- Winner board tracked
+- Styling
 
+## Future features:
+- Winners leader board tracked
+- Chat!
+- Who's got next?
+- Abandoned game tracking
+- Rock-Paper-Scissors-Lizard-Spock!
+- Allow players to register and login in
+
+- - -
 ### Instructions
 
 * Create a game that suits this user story:
-
-  * Only two users can play at the same time.
-
-  * Both players pick either `rock`, `paper` or `scissors`. After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
-
-  * The game will track each player's wins and losses.
-
-  * Throw some chat functionality in there! No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
-
-  * Styling and theme are completely up to you. Get Creative!
-
-  * Deploy your assignment to Github Pages.
 
 - - -
 
@@ -55,21 +53,5 @@ Add a `README.md` to your repository describing the project. Here are some resou
 
 * [Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
 - - -
-
-### Add To Your Portfolio
-
-After completing the homework please add the piece to your portfolio. Make sure to add a link to your updated portfolio in the comments section of your homework so the TAs can easily ensure you completed this step when they are grading the assignment. To receive an 'A' on any assignment, you must link to it from your portfolio.
-
-- - -
-
-### Additional Practice and Support
-
-* If you find your skills lacking in any of the subjects we taught you, look at your instructor's in class repository. 
-
-  * Find the exercises that you did in class and redo them from scratch. It might seem redundant at first, but this will help edify the material.
-
-  * You can also watch videos of this all of our past lectures--we've saved these to the repo.
-
-*If you have any questions about this project or the material we have covered, please post them in the community channels in slack so that your fellow developers can help you! If you're still having trouble, you can come to office hours for assistance from your instructor and TAs.
 
   **Good Luck!**
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 var config = {
     apiKey: "AIzaSyA69XoF-OD4_jkWwTS3cOwNinWH7jtxSFM",
     authDomain: "congenial-umbrella.firebaseapp.com",
-    databaseURL: "https://congenial-umbrella.firebaseio.com",
+    databaseURL: "https:// congenial-umbrella.firebaseio.com",
     projectId: "congenial-umbrella",
     storageBucket: "congenial-umbrella.appspot.com",
     messagingSenderId: "86042197262"
@@ -19,17 +19,20 @@ var registeredName = "";
 gameSetup();
 
 function gameSetup() {
-    //deactivate play buttons until start buttons
-    disableButons();
+    disableGameButtons();
 }
 
 
-function disableButons() {
-    //deactivate play buttons until start buttons
+function disableGameButtons() {
+    // deactivate game play buttons until player joins a game
     $(".rpsls").attr("disabled", true);
 }
 
-//Validate new or existing users
+// add chat tracking... some kind of push-pop most recent 5-10 comments
+// any user can chat, not just players...
+
+
+// Validate new or existing users
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         // User is signed in.
@@ -51,14 +54,14 @@ firebase.auth().onAuthStateChanged(function(user) {
             } else {
                 console.log("User Exists")
                 playerName = snapshot.child("displayName").val();
-                // $("#registration").empty();
+
                 $("#player-name").text("Welcome " + playerName + "!")
                 nameExists = (playerName !== null && playerName > "");
             }
 
-            //See if we need to set the name
+            // See if we need to set the name
             if (!nameExists || (playerName !== registeredName && registeredName > "")) {
-                //New Name || Name Change
+                // New Name || Name Change
                 playerRef.child("displayName").set(registeredName);
             }
         });
@@ -66,16 +69,15 @@ firebase.auth().onAuthStateChanged(function(user) {
         // ...
     } else {
         // User is signed out.
+        // if they were mid-game, declare the other player winner by forfeit
+        // track forfeits to player stats
         // ... show registration
     }
     // ...
 });
-// var playerCount = 0;
-// push "child added"
 
-// Whenever a user clicks the register button
+// Visitor registers to play
 $("#register").on("click", function(event) {
-    // Prevent form from submitting
     event.preventDefault();
 
     registeredName = $("#register-name").val();
@@ -88,201 +90,186 @@ $("#register").on("click", function(event) {
         console.log(errorMessage);
 
     });
-    //this action triggers our player validation check...
+    // this action triggers our player validation check...
     database.ref("players").child(userId).child('lastLogin').set(firebase.database.ServerValue.TIMESTAMP);
 });
 
 var gameRef = database.ref("game");
 
-// var playerJoins = gameRef.child("join");
-// playerJoins.child("player-count").set(0);
+// Player joins game
+gameRef.child("join").on('value', function(snapshot) {
+    if (!snapshot.exists()) {
+        return;
+    }
 
-// console.log(userId + "played");
+    var newPlayer = snapshot.child("userId").val();
+    if (newPlayer === null || newPlayer !== userId) {
+        // triggered by something other than the player joining
+        // OR player joined is not us...
+        // exit out, because both players don't need to run checks
+        return;
+    }
 
+    gameRef.once('value', function(snapshot) {
+        // Set first player
+        if (!snapshot.hasChild("playerOne")) {
+            playerID = "playerOne";
+        } else {
+            if (snapshot.child("playerOne").child("userId").val() === newPlayer) {
+                playerID = "playerOne";
+            } else {
+                // Player One already exists
+                // Set second player
+                if (!snapshot.hasChild("playerTwo")) {
+                    playerID = "playerTwo";
+                } else {
+                    if (snapshot.child("playerTwo").child("userId").val() === newPlayer) {
+                        playerID = "playerTwo";
+                    } else {
+                        // Player Two already exists
+                        // Player is a watcher
+                        playerID = "Watcher";
+                    }
+                }
+            }
+        }
+
+        if (playerID !== "Watcher") {
+            // add our player to the game boards
+            var wins = 0
+            gameRef.child(playerID).set({
+                userId,
+                wins
+            });
+            console.log("Player joined");
+        } else {
+            // Add some code to cue watchers up for "Got Next..."
+        }
+
+        if (playerID !== "Watcher") {
+            // activate buttons
+            $(".rpsls").attr("disabled", null);
+        } else {
+            // deactivate buttons
+            $(".rpsls").attr("disabled", true);
+        }
+        // Not sure if its better to remove this sooner...
+        gameRef.child("join").remove();
+    });
+
+});
+
+// Score player moves
 gameRef.on('value', function(snapshot) {
     if (!snapshot.exists()) {
         return;
     }
 
-    // Score player moves
-    var p1, p2;
-    var p1Choice, p2Choice;
-    var p1wins, p2wins;
+    var p1, p2; // player references
+    var p1Choice, p2Choice; // player choices
+    var p1wins, p2wins; // player win counts
 
     if (snapshot.hasChild("playerOne")) {
-        // debugger;
         p1 = snapshot.child("playerOne");
         if (p1.hasChild("choice")) {
             p1Choice = p1.child("choice").val();
         }
         if (p1.hasChild("wins")) {
             p1wins = p1.child("wins").val();
-            // } else {
-            //     p1wins = 0
         }
     }
 
     if (snapshot.hasChild("playerTwo")) {
-        // debugger;
         p2 = snapshot.child("playerTwo");
         if (p2.hasChild("choice")) {
             p2Choice = p2.child("choice").val();
         }
         if (p2.hasChild("wins")) {
             p2wins = p2.child("wins").val();
-            // } else {
-            //     p2wins = 0
         }
     }
 
+    // both players ready?
     if (typeof p1Choice != "undefined" && typeof p2Choice != "undefined") {
-        // console.log(p1Choice)
-        // console.log(p2Choice)
-        // clear the players choices so we don't trigger a loops
+
+        // clear the player's choices so we don't trigger a scoring loop
         gameRef.child("playerOne").child("choice").remove();
         gameRef.child("playerTwo").child("choice").remove();
 
         if (p1Choice === p2Choice) {
             $("#action").text("Tie!  Play again...");
         }
-        //Rock beats Scissors
+
+        // Player 1 winning scenarios
+        // Rock beats Scissors
         if (p1Choice === "rock" && p2Choice === "scissors") {
             $("#action").text("Rock beats Scissors... Player 1 wins");
             p1wins += 1;
         }
-        //Scissors beats Paper
+        // Scissors beats Paper
         if (p1Choice === "scissors" && p2Choice === "paper") {
             $("#action").text("Scissors beats Paper... Player 1 wins");
             p1wins += 1;
         }
-        //Paper beats Rock
+        // Paper beats Rock
         if (p1Choice === "paper" && p2Choice === "rock") {
             $("#action").text("Paper beats Rock... Player 1 wins");
             p1wins += 1;
         }
 
-        //Rock beats Scissors
+        // Player 2 winning scenarios
+        // Rock beats Scissors
         if (p2Choice === "rock" && p1Choice === "scissors") {
             $("#action").text("Rock beats Scissors... Player 2 wins");
             p2wins += 1;
         }
-        //Scissors beats Paper
+        // Scissors beats Paper
         if (p2Choice === "scissors" && p1Choice === "paper") {
             $("#action").text("Scissors beats Paper... Player 2 wins");
             p2wins += 1;
         }
-        //Paper beats Rock
+        // Paper beats Rock
         if (p2Choice === "paper" && p1Choice === "rock") {
             $("#action").text("Paper beats Rock... Player 2 wins");
             p2wins += 1;
         }
-        console.log(p1wins)
-        console.log(p2wins)
+
         gameRef.child("playerOne").update({ wins: p1wins });
         gameRef.child("playerTwo").update({ wins: p2wins });
 
-        //re-activate buttons
+        if (p1wins >= 3 || p2wins >= 3) {
+            // Declare final winner
+            // Add winner to leader board
+            // Check who's got next!
+        }
+
+        // re-activate buttons
         $(".rpsls").attr("disabled", null);
 
     }
 
 });
 
-gameRef.child("join").on('value', function(snapshot) {
-    // Player joins game
-    if (!snapshot.exists()) {
-        return;
-    }
-    var newPlayer = snapshot.child("userId").val();
-    if (newPlayer === null || newPlayer !== userId) {
-        // something other than player joining...
-        // OR player joined is someone else - we don't both need to process
-        return;
-    }
-
-    gameRef.once('value', function(snapshot) {
-        if (!snapshot.hasChild("playerOne")) {
-            // gameRef.child("playerOne").set(newPlayer);
-            playerID = "playerOne";
-        } else {
-            // console.log(snapshot.child("playerOne").child("userId"))
-            // console.log(newPlayer)
-            if (snapshot.child("playerOne").child("userId").val() === newPlayer) {
-                playerID = "playerOne";
-            } else {
-
-                // console.log("Player One already exists");
-
-                if (!snapshot.hasChild("playerTwo")) {
-
-                    // gameRef.child("playerTwo").set(newPlayer);
-                    playerID = "playerTwo";
-                } else {
-                    if (snapshot.child("playerTwo").child("userId").val() === newPlayer) {
-                        playerID = "playerTwo";
-                    } else {
-                        // console.log("Player Two already exists");
-                        //player is a watcher
-                        playerID = "Watcher";
-                    }
-                }
-            }
-        }
-        var wins = 0
-        gameRef.child(playerID).set({
-            userId,
-            wins
-        });
-        console.log("Player joined");
-        // console.log("Ready " + playerName);
-        if (playerID !== "Watcher") {
-            //activate buttons
-            $(".rpsls").attr("disabled", null);
-        } else {
-            //deactivate buttons
-            $(".rpsls").attr("disabled", true);
-        }
-        gameRef.child("join").remove();
-    });
-
-});
-
-// Whenever a user clicks the start button
+// Whenever a user clicks the "Fight" button
 $("#start").on("click", function(event) {
-    // Prevent form from submitting
     event.preventDefault();
-
     console.log("start");
-
     gameRef.child("join").child("userId").set(userId);
-
 });
 
-
+// Handle the player's game choices
 $(".rpsls").on("click", function(event) {
-    disableButons();
     event.preventDefault();
-    // disabe players buttons until both have played
 
-    // console.log(playerID + " playeD");
-    // $(this).val:
-    //  rock
-    //  paper
-    //  scissors
-    //  lizard - TBD
-    //  spock - TBD
-    // console.log(userId);
-    // console.log(playerID);
-    // console.log($(this).val());
-    // gameRef.child(playerID).set({
-    //     userId,
+    // disabe this player's buttons until both have played
+    disableGameButtons();
 
-    //     choice: $(this).val()
-
-    // });
+    // $(this) values:
+    // rock
+    // paper
+    // scissors
+    // lizard - TBD
+    // spock - TBD
     gameRef.child(playerID).update({ choice: $(this).val() });
-    // gameRef.child("playerTwo").child("wins").update(p2wins);
 
-    // console.log(gameRef.child("plays").val());
-    // gameRef.child(playerID).child("choice").set($(this).val());
 });
